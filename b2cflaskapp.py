97a7984c7e457e787db5ff33188c16c4f669a0fa
adammartin <ebondark.od@gmail.com>
2018-02-14 18:47:09 -0500

    breaking app apart into sane pieces

--- b2cflaskapp.py
+++ b2cflaskapp.py
@@ -1,5 +1,4 @@
-from flask import Flask, redirect, url_for, session, request, jsonify, render_template
-from flask_oauthlib.client import OAuth, OAuthException
+from flask import Flask, redirect, url_for, session, request, render_template
 
 from jose import jws
 import json
@@ -8,44 +7,25 @@ import requests
 from logging import Logger
 import uuid
 
+import yaml
+
+from microsoft_client import microsoft_client, keys
+
+with open("config/config.yml", 'r') as ymlfile:
+    config = yaml.load(ymlfile)
+
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'development'
-oauth = OAuth(app)
-
-# === Update these values ===========
-tenant_id = 'fabrikamb2c.onmicrosoft.com'
-client_id = 'fdb91ff5-5ce6-41f3-bdbd-8267c817015d'
-client_secret = r'X330F3#92!z614M4'
-policy_name = 'b2c_1_susi'
-# ===================================
-
-scopes = 'openid ' + client_id
-
-core_url = 'https://login.microsoftonline.com/tfp/' + tenant_id +'/' + policy_name
-token_url = core_url + '/oauth2/v2.0/token'
-authorize_url = core_url + '/oauth2/v2.0/authorize'
-keys_url = core_url + '/discovery/keys'
 
 # This sample loads the keys on boot, but for production
-# the keys should be refreshed either periodically or on 
+# the keys should be refreshed either periodically or on
 # jws.verify fail to be able to handle a key rollover
-keys_raw = requests.get(keys_url).text
-keys = json.loads(keys_raw)	
+keys = keys(config)
 
 # Put your consumer key and consumer secret into a config file
 # and don't check it into github!!
-microsoft = oauth.remote_app(
-	'microsoft',
-	consumer_key=client_id,
-	consumer_secret=client_secret,
-	request_token_params={'scope': scopes },
-	base_url='http://ignore',  # We won't need this
-	request_token_url=None,
-	access_token_method='POST',
-	access_token_url=token_url,
-	authorize_url=authorize_url
-)
+microsoft = microsoft_client(config, app)
 
 
 @app.route('/')
@@ -63,7 +43,7 @@ def login():
 	session['state'] = guid
 
 	return microsoft.authorize(callback=url_for('authorized', _external=True), state=guid)
-	
+
 @app.route('/logout', methods = ['POST', 'GET'])
 def logout():
 	session.pop('microsoft_token', None)
@@ -77,22 +57,22 @@ def authorized():
 
 	if response is None:
 		return "Access Denied: Reason=%s\nError=%s" % (
-			response.get('error'), 
+			response.get('error'),
 			request.get('error_description')
 		)
-		
+
 	# Check response for state
 	print("Response: " + str(response))
 	if str(session['state']) != str(request.args['state']):
 		raise Exception('State has been messed with, end authentication')
-		
+
 	# Okay to store this in a local variable, encrypt if it's going to client
 	# machine or database. Treat as a password.
-	access_token = response['access_token'] 
+	access_token = response['access_token']
 	session['microsoft_token'] = (access_token, '')
 	session['claims'] = json.loads(jws.verify(access_token, keys, algorithms=['RS256']))
 
-	return redirect(url_for('me')) 
+	return redirect(url_for('me'))
 
 @app.route('/me')
 def me():
---
+++ microsoft_client.py
@@ -0,0 +1,33 @@
+from flask_oauthlib.client import OAuth, OAuthException
+import json, requests
+
+def microsoft_client(config, app):
+    return OAuth(app).remote_app(
+        'microsoft',
+        consumer_key=config['client_id'],
+        consumer_secret=config['client_secret'],
+        request_token_params={'scope': _scopes(config) },
+        base_url='http://ignore',  # We won't need this
+        request_token_url=None,
+        access_token_method='POST',
+        access_token_url=_token_url(config),
+        authorize_url=_authorize_url(config)
+    )
+
+def keys(config):
+    return json.loads(requests.get(_keys_url(config)).text)
+
+def _keys_url(config):
+    return _core_url(config) + '/discovery/keys'
+
+def _core_url(config):
+    return 'https://login.microsoftonline.com/tfp/' + config['tenant_id'] +'/' + config['policy_name']
+
+def _scopes(config):
+    return 'openid ' + config['client_id']
+
+def _token_url(config):
+    return _core_url(config) + '/oauth2/v2.0/token'
+
+def _authorize_url(config):
+    return _core_url(config) + '/oauth2/v2.0/authorize'
